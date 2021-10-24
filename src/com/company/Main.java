a12304253d838e28799d444422db4969f1b1b94c
Oleksandr Kharchenko <my.the.best.logic@gmail.com>
2021-10-25 02:53:36 +0300

    1_2 end

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,22 @@
 package com.company;
 
 
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Task 1.2");
-
+        for(int i = 2; i <= 100; i ++){
+            boolean isPrime = true;
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
     }
 }
