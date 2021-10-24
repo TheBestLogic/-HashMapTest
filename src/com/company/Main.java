e3f178cd7f09aa68a4fcfd15dfe36ed3819f9b2c
Oleksandr Kharchenko <my.the.best.logic@gmail.com>
2021-10-25 02:35:30 +0300

    1_2 start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Task 1.1");
-        int ARRAY_LENGTH = 50;
-        double[] Array = new double[ARRAY_LENGTH];
-        for (int i = 0; i < ARRAY_LENGTH; ++i)
-        {
-            Array[i] = Math.random();
-        }
-        double buf = 0;
-        for (int i = ARRAY_LENGTH- 1; i >= 1; --i)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if(Array[j] > Array[j + 1])
-                {
-                    buf = Array[j];
-                    Array[j] = Array[j+1];
-                    Array[j+1] = buf;
-                }
+        System.out.println("Task 1.2");
 
-            }
-        }
-        System.out.println("Array[0] = " + Array[0] + "\nArray[ARRAY_LENGTH-1] = " + Array[ARRAY_LENGTH-1]);
     }
 }
