0194e830f86925d7985e5019a43bb10392b19330
Oleksandr Kharchenko <my.the.best.logic@gmail.com>
2021-10-25 01:35:35 +0300

    1_1 start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Start");
+        System.out.println("Task 1.1");
         int ARRAY_LENGHT = 50;
-        double max = 0, min = 1, avg = 0;
         double[] Array = new double[ARRAY_LENGHT];
         for (int i = 0; i < ARRAY_LENGHT; ++i)
         {
             Array[i] = Math.random();
-            if( max < Array[i] )
-                max = Array[i];
-            if( min > Array[i])
-                min = Array[i];
-            avg += Array[i];
         }
-        avg /= ARRAY_LENGHT;
-        System.out.println("Max = "+ max + ";\nMin = " + min + ";\nAvg = "+ avg +";");
     }
 }
